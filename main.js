a96724657f309873b403cbfe11bf281448a641d1
Ashish Kumar <ashishkmr@hotmail.com>
2017-06-04 04:24:54 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,9 +2,6 @@
     $(document).ready(function() {
       $('.more-link').click(function(e) {
       	e.preventDefault();
-        $('.more-link').each(function() {
-          $(this).closest('article').css({"width":"", "height":"", "top": "", "left" : ""});
-        });
         var parent = $(this).closest('article');
         var id = parent.attr('id');
         var n = id.lastIndexOf('-');
@@ -17,10 +14,7 @@
         		  "post-id" : post_id
           },
           success: function (data) {         
-          	alert('here');
-            var current_width = parent.width()*2;
-            var current_height = parent.height()*1.5;
-            parent.css({"width":current_width, "height":current_height});
+          	$("#myModal").modal('show');
           }
       	});
       });
